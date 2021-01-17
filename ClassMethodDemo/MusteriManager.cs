e1545a6203ea475c77c2b25d5463ba7d46064ec9
Ahmet Emre Apo <apoahmetemre@gmail.com>
2021-01-18 01:04:14 +0300

    Banka müşterisi ekleme, listeleme ve silme işlemleri.

---
+++ ClassMethodDemo/MusteriManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class MusteriManager
+    {
+        public static void MusteriEkleme(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " " + musteri.Soyad + "sisteme eklenmiştir.");
+        }
+
+        public static void MusteriListeleme(Musteri[] musteriListe)
+        {
+            foreach (Musteri musteri in musteriListe)
+            {
+                Console.WriteLine(musteri.Id + musteri.Ad + musteri.Soyad);
+            }
+        }
+
+        public static void MusteriSilme(Musteri musteri)
+        {
+            Console.WriteLine("ID'si " + musteri.Id + "olan " + musteri.Ad + musteri.Soyad + "sistemden silinmiştir.");
+        }
+    }
+}
--- ClassMethodDemo/Program.cs
+++ ClassMethodDemo/Program.cs
@@ -6,7 +6,38 @@ namespace ClassMethodDemo
     {
         static void Main(string[] args)
         {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 34874;
+            musteri1.Ad = "Ahmet Emre";
+            musteri1.Soyad = "Apo";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 34875;
+            musteri2.Ad = "Eren";
+            musteri2.Soyad = "Aydın";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 34876;
+            musteri3.Ad = "Ceren";
+            musteri3.Soyad = "Güler";
+
+
+
             
+
+
+
+
+
         }
+
+
+    }
+
+    class Musteri
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
     }
 }
